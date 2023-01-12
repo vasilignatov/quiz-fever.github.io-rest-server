43dd22a5c26c801e75a54d59aadb3129900da5fa
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-01-12 15:48:07 +0200

    Fixed bug with login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,19 +3,19 @@ const userService = require('../services/user-service');
 const { COOKIE_NAME } = require('../constants');
 
 router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
+    const {  email,  password } = req.body;
 
     try {
         await userService.register({ username, email, password });
 
         let { user, token } = await userService.login({ username, password });
-
-        console.log(req.user);
         
         res.cookie(COOKIE_NAME, token);
+
         res.json({
-            sessionToken: token,
             objectId: user._id,
+            sessionToken: token,
+            username: user.username
         });
     } catch (err) {
         res.json({
@@ -32,12 +32,16 @@ router.post('/login', async (req, res) => {
         let { user, token } = await userService.login({ username, password });
 
         res.json({
-            _id: user._id,
             email: user.email,
-            authToken: token
+            objectId: user._id,
+            sessionToken: token,
+            username: user.username
         });
     } catch (error) {
-
+        res.json({
+            type: 'error',
+            message: 'Login error: ' + err
+        });
     }
 
 });
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,13 +2,10 @@ const jwt = require('../utils/jwtUtils');
 const { COOKIE_NAME, JWT_SECRET } = require('../constants');
 
 exports.auth = async function (req, res, next) {
-    let token = req.headers['x-parse-session-token']; 
-    
-    console.log('Req header auth: ', token);
+    let token = req.headers['x-parse-session-token'];
 
-    if (token != undefined) {
+    if (token) {
         try {
-            console.log('In token!!!!');
             const decoded = await jwt.verify(token, JWT_SECRET);
             req.user = decoded;
             return next();
