fc534cef60867dcc3070d74a15d30d9e281c615b
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-01-12 14:47:54 +0200

    Add auth controller

--- src/config/express-config.js
+++ src/config/express-config.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { auth } = require('../middlewares/authMiddleware');
 
 const routes = require('../routes');
 
 function config(app) {
     app.use(express.json());
+    app.use(auth);
     app.use(cors());
+    app.use(cookieParser());
     app.use(routes);
 }
 
--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,9 +1,51 @@
 const router = require('express').Router();
+const userService = require('../services/user-service');
+const { COOKIE_NAME } = require('../constants');
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
 
-    res.json({ok:true});
+    try {
+        await userService.register({ username, email, password });
+
+        let { user, token } = await userService.login({ username, password });
+
+        console.log(req.user);
+        
+        res.cookie(COOKIE_NAME, token);
+        res.json({
+            sessionToken: token,
+            objectId: user._id,
+        });
+    } catch (err) {
+        res.json({
+            type: 'error',
+            message: 'Register error: ' + err
+        });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    let { username, password } = req.body;
+
+    try {
+        let { user, token } = await userService.login({ username, password });
+
+        res.json({
+            _id: user._id,
+            email: user.email,
+            authToken: token
+        });
+    } catch (error) {
+
+    }
+
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie(COOKIE_NAME);
+    res.json({ ok: true });
+    req.head
 });
 
 module.exports = router;
