d3ba9589f1e6e92e29e21a5aa154ff0964f13816
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-01-18 11:17:16 +0200

    Create Solution Model

---
+++ src/models/Solution.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const solutionSchema = new mongoose.Schema({
+    objectId: {
+        typeof: mongoose.Schema.Types.ObjectId
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    quiz: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    total: String
+});
+
+solutionSchema.pre('save', function() {
+    this.objectId = this._id;
+});
+
+const Solution = mongoose.model('Solution', solutionSchema);
+module.exports = Solution;
