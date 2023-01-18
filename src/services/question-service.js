4aca6d9649bf742211c69e1c3d91f6e6fd2432a7
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-01-18 23:11:52 +0200

    Fixed bug with getQuestionsById query

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,5 @@
 const Question = require('../models/Question');
+const { ObjectId } = require('mongoose').Types;
 
 // CRUD
 exports.createQst = (questionData) => Question.create(questionData);
@@ -7,5 +8,11 @@ exports.updateQst = (quiestionId, data) => Question.findByIdAndUpdate(quiestionI
 
 exports.deleteQst = (quiestionId, data) => Question.findByIdAndUpdate(quiestionId, data);
 
-exports.getQuestionsByQuizId = (quizId) => Question.find({_id: quizId}).lean();
+exports.getQuestionsByQuizId = (quizId, ownerId) =>{
+    return Question.find({
+        quiz: new ObjectId(quizId),
+        owner: new ObjectId(ownerId)
+    })
+    .lean();
+}
 
