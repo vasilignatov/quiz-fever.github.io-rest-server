3933b6411b0dd5c5addc9972c54e2b554f5f9c99
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-01-18 23:12:29 +0200

    Create Solution controller

---
+++ src/controllers/solution-controller.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const solutionService = require('../services/solution-service');
+
+
+// Get solutions by userId or quizID
+router.get('/Solution', async (req, res) => {
+    try {
+        const owner = JSON.parse(req.query.where).owner;
+        if (owner.className == '_User') {
+            const result = await solutionService.getSolutionsByUserId(owner.objectId);
+            res.json(result);
+        } else if (owner.className == 'Quiz') {
+            const result = await solutionService.getSolutionsByQuizId(owner.objectId);
+            res.json(result);
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
+
+// Submit solution
+router.post('/Solution', async (req, res) => {
+    try {
+        let { total, correct, owner, quiz } = req.body;
+        quiz = quiz.objectId;
+        
+        await solutionService.create({ total, correct, owner, quiz });
+        
+        res.json({ok: true});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error });
+    }
+});
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const authController = require('./controllers/auth-controller');
 const quizController = require('./controllers/quiz-controller');
 const questionController = require('./controllers/question-controller');
+const solutionController = require('./controllers/solution-controller');
 
 router.use('/users', authController);
 router.use('/classes', quizController);
 router.use('/classes', questionController);
+router.use('/classes', solutionController);
 
 module.exports = router;
